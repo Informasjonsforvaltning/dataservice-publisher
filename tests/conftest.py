79ffea216b581c0e2fd628a30ad777ba547eac98
Stig B. Dørmænen <stigbd@gmail.com>
2020-04-22 15:32:37 +0200

    Make deploy-workflow deploy the image build by pytest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,7 +21,7 @@ def is_responsive(url: Any) -> Any:
 def http_service(docker_ip: Any, docker_services: Any) -> Any:
     """Ensure that HTTP service is up and responsive."""
     # `port_for` takes a container port and returns the corresponding host port
-    port = docker_services.port_for("dataservicepublisher", 8080)
+    port = docker_services.port_for("dataservice-publisher", 8080)
     url = "http://{}:{}".format(docker_ip, port)
     docker_services.wait_until_responsive(
         timeout=30.0, pause=0.1, check=lambda: is_responsive(url)
